ecd0673065f3f9a60eee2eb74998eaaad5cfb6dd
“nimraaslam3132@gmail.com <nimraaslam3132@gmail.com>
2024-04-28 12:10:55 +0500

    training data

--- src/cnnClassifier/constants/__init__.py
+++ src/cnnClassifier/constants/__init__.py
@@ -1,4 +1,4 @@
 from pathlib import Path
 
-CONFIG_FILE_PATH = Path(r"E:\end-to-end-Deep-learning-project-on-Kidney-Disease-Classification\config\config.yaml")
-PARAMS_FILE_PATH = Path(r"E:\end-to-end-Deep-learning-project-on-Kidney-Disease-Classification\params.yaml")
+CONFIG_FILE_PATH = Path("E:\end-to-end-Deep-learning-project-on-Kidney-Disease-Classification\config\config.yaml")
+PARAMS_FILE_PATH = Path("E:\end-to-end-Deep-learning-project-on-Kidney-Disease-Classification\params.yaml")
